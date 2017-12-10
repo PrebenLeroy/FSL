cbdb8bf7ab254237a75f446a85c42fffac5c2575
PrebenLeroy <preben.leroy@live.be>
2017-12-10 15:53:29 +0100

    first commit

---
+++ calculations/Calculator.js
@@ -0,0 +1,15 @@
+class Calculator
+{
+
+    calculateDurability(){
+
+    }
+
+    calculateAverageLoadingstops(totalKilometerYear, averageKilometerPerTrip,
+         averageLoadingStops,  totalLoadingCapacityYear)
+     {
+        return totalLoadingCapacityYear/ (totalKilometerYear / averageKilometerPerTrip) / averageLoadingStops;
+     }
+
+     
+} export class {Calculator}
---
+++ calculations/Diesel.js
@@ -0,0 +1,26 @@
+class Diesel{
+    calculateFuelScanLight(currentFuel, vehicletype){
+        if(currentFuel.toLowerCase() === "diesel" && (vehicletype === "SCV" || vehicletype === "MCV")){
+            return "Referentie";
+        } else if (vehicletype === "SCV" || vehicletype === "MCV"){
+            /*if(Wegingsfactoren.Gemiddelde belading van het voertuig != 0){
+                return (
+            Wegingsfactoren.duurzaamheidBronnenWaardeDiesel
+            * DuurzaamheidVanBronnen%)
+            +(Wegingsfactoren.Duurzaamheid (TonCO2/ Ton vervoerde goederen)Waardediesel
+            *Duurzaamheid(TONCO2/TONVERVOERDEGOEDEREN)%)
+            +(Wegingsfactoren.Kostprijs van brandstofDieselWaarde 
+            *Kostprijs van brandstof%)
+            +(Wegingsfactoren.Houdbaarheid van brandstofkwaliteitDieselWaarde
+            *Houdbaarheid van brandstofkwaliteit%)
+            +(Wegingsfactoren.SnelheidBrandstoftankenDieselWaarde*SnelheidBrandstoftanken%)
+            +(Wegingsfactoren.GeluidsOverlastDieselWaarde*geluidsoverlast%);
+                }else{
+                 return 0; 
+                }*/
+            return 0;
+        } else{
+                return "Out of Scope";
+        }
+    }
+}
